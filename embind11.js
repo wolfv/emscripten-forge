42231505c93b9af97bbdc0a6098accfca0e490d6
DerThorsten <derthorstenbeier@gmail.com>
2022-03-16 08:47:19 +0000

    Deploying to gh-pages from @ emscripten-forge/recipes@d1ce565bcca7205fafd840a213d28ca8aeaa5497 🚀

--- embind11.js
+++ embind11.js
@@ -1,125 +1,197 @@
-<!doctype html>
-<html lang="en-us">
-<head>
-  <title>python3.11-wasm embeded via pybind11 build by boa</title>
-</head>
-<body>
 
+  var Module = typeof globalThis.Module !== 'undefined' ? globalThis.Module : {};
 
-  <!-- <h3>python3.11-wasm embeded via pybind11 build by boa</h3> -->
-  <form name="myform">
-  <td><textarea name="inputtext"></textarea></td>
-   <td><canvas name="mycanvas" id="mycanvas" width="1000" height="200"></canvas></td> 
+  if (!Module.expectedDataFileDownloads) {
+    Module.expectedDataFileDownloads = 0;
+  }
 
-  Output:
-  <td><textarea name="outputtext"></textarea></td> 
-  </form>
+  Module.expectedDataFileDownloads++;
+  (function() {
+    // When running as a pthread, FS operations are proxied to the main thread, so we don't need to
+    // fetch the .data bundle on the worker
+    if (Module['ENVIRONMENT_IS_PTHREAD']) return;
+    var loadPackage = function(metadata) {
 
+      var PACKAGE_PATH = '';
+      if (typeof window === 'object') {
+        PACKAGE_PATH = window['encodeURIComponent'](window.location.pathname.toString().substring(0, window.location.pathname.toString().lastIndexOf('/')) + '/');
+      } else if (typeof process === 'undefined' && typeof location !== 'undefined') {
+        // web worker
+        PACKAGE_PATH = encodeURIComponent(location.pathname.toString().substring(0, location.pathname.toString().lastIndexOf('/')) + '/');
+      }
+      var PACKAGE_NAME = 'embind11.data';
+      var REMOTE_PACKAGE_BASE = 'embind11.data';
+      if (typeof Module['locateFilePackage'] === 'function' && !Module['locateFile']) {
+        Module['locateFile'] = Module['locateFilePackage'];
+        err('warning: you defined Module.locateFilePackage, that has been renamed to Module.locateFile (using your locateFilePackage for now)');
+      }
+      var REMOTE_PACKAGE_NAME = Module['locateFile'] ? Module['locateFile'](REMOTE_PACKAGE_BASE, '') : REMOTE_PACKAGE_BASE;
 
-  <script type="application/javascript" src="embed.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.32.0/codemirror.min.js"></script>
-  <script src='https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.13.4/mode/python/python.js'></script>
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.32.0/codemirror.css" />
-  <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.13.4/theme/monokai.css'/>
-  <script type="application/javascript">
+      var REMOTE_PACKAGE_SIZE = metadata['remote_package_size'];
+      var PACKAGE_UUID = metadata['package_uuid'];
 
-
-    outputtext = document.myform.outputtext
-    outputtext.value = ""
-
-    var editor = CodeMirror.fromTextArea(document.myform.inputtext, {
-      lineNumbers: true,
-      mode: 'python',
-      //  add theme attribute like so:
-      theme: 'monokai',
-      extraKeys: {
-            "Tab": function(cm){
-              cm.replaceSelection("   " , "end");
+      function fetchRemotePackage(packageName, packageSize, callback, errback) {
+        if (typeof process === 'object' && typeof process.versions === 'object' && typeof process.versions.node === 'string') {
+          require('fs').readFile(packageName, function(err, contents) {
+            if (err) {
+              errback(err);
+            } else {
+              callback(contents.buffer);
             }
-           }
-    });
+          });
+          return;
+        }
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', packageName, true);
+        xhr.responseType = 'arraybuffer';
+        xhr.onprogress = function(event) {
+          var url = packageName;
+          var size = packageSize;
+          if (event.total) size = event.total;
+          if (event.loaded) {
+            if (!xhr.addedTotal) {
+              xhr.addedTotal = true;
+              if (!Module.dataFileDownloads) Module.dataFileDownloads = {};
+              Module.dataFileDownloads[url] = {
+                loaded: event.loaded,
+                total: size
+              };
+            } else {
+              Module.dataFileDownloads[url].loaded = event.loaded;
+            }
+            var total = 0;
+            var loaded = 0;
+            var num = 0;
+            for (var download in Module.dataFileDownloads) {
+            var data = Module.dataFileDownloads[download];
+              total += data.total;
+              loaded += data.loaded;
+              num++;
+            }
+            total = Math.ceil(total * Module.expectedDataFileDownloads/num);
+            if (Module['setStatus']) Module['setStatus']('Downloading data... (' + loaded + '/' + total + ')');
+          } else if (!Module.dataFileDownloads) {
+            if (Module['setStatus']) Module['setStatus']('Downloading data...');
+          }
+        };
+        xhr.onerror = function(event) {
+          throw new Error("NetworkError for: " + packageName);
+        }
+        xhr.onload = function(event) {
+          if (xhr.status == 200 || xhr.status == 304 || xhr.status == 206 || (xhr.status == 0 && xhr.response)) { // file URLs can return 0
+            var packageData = xhr.response;
+            callback(packageData);
+          } else {
+            throw new Error(xhr.statusText + " : " + xhr.responseURL);
+          }
+        };
+        xhr.send(null);
+      };
 
-    var logeditor = CodeMirror.fromTextArea(document.myform.outputtext, {
-      lineNumbers: false,
-      readOnly: true,
-      //  add theme attribute like so:
-      theme: 'monokai'
-    });
-    logeditor.setSize(null, 200);
+      function handleError(error) {
+        console.error('package error:', error);
+      };
 
-    
-    const print = (text) => {
-      
-      logeditor.replaceRange(text+"\n", CodeMirror.Pos(logeditor.lastLine()))
-      editor.scrollTo(CodeMirror.Pos(logeditor.lastLine()));
+      var fetchedCallback = null;
+      var fetched = Module['getPreloadedPackage'] ? Module['getPreloadedPackage'](REMOTE_PACKAGE_NAME, REMOTE_PACKAGE_SIZE) : null;
+
+      if (!fetched) fetchRemotePackage(REMOTE_PACKAGE_NAME, REMOTE_PACKAGE_SIZE, function(data) {
+        if (fetchedCallback) {
+          fetchedCallback(data);
+          fetchedCallback = null;
+        } else {
+          fetched = data;
+        }
+      }, handleError);
+
+    function runWithFS() {
+
+      function assert(check, msg) {
+        if (!check) throw msg + new Error().stack;
+      }
+Module['FS_createPath']("/", "home", true, true);
+Module['FS_createPath']("/home", "runner", true, true);
+Module['FS_createPath']("/home/runner", "micromamba", true, true);
+Module['FS_createPath']("/home/runner/micromamba", "envs", true, true);
+Module['FS_createPath']("/home/runner/micromamba/envs", "emforgetest", true, true);
+Module['FS_createPath']("/home/runner/micromamba/envs/emforgetest", "lib", true, true);
+Module['FS_createPath']("/home/runner/micromamba/envs/emforgetest/lib", "python3.11", true, true);
+Module['FS_createPath']("/home/runner/micromamba/envs/emforgetest/lib/python3.11", "site-packages", true, true);
+
+      /** @constructor */
+      function DataRequest(start, end, audio) {
+        this.start = start;
+        this.end = end;
+        this.audio = audio;
+      }
+      DataRequest.prototype = {
+        requests: {},
+        open: function(mode, name) {
+          this.name = name;
+          this.requests[name] = this;
+          Module['addRunDependency']('fp ' + this.name);
+        },
+        send: function() {},
+        onload: function() {
+          var byteArray = this.byteArray.subarray(this.start, this.end);
+          this.finish(byteArray);
+        },
+        finish: function(byteArray) {
+          var that = this;
+          
+          Module['FS_createPreloadedFile'](this.name, null, byteArray, true, true, function() {
+            Module['removeRunDependency']('fp ' + that.name);
+          }, function() {
+            if (that.audio) {
+              Module['removeRunDependency']('fp ' + that.name); // workaround for chromium bug 124926 (still no audio with this, but at least we don't hang)
+            } else {
+              err('Preloading file ' + that.name + ' failed');
+            }
+          }, false, true); // canOwn this data in the filesystem, it is a slide into the heap that will never change
+
+          this.requests[this.name] = null;
+        }
+      };
+
+      var files = metadata['files'];
+      for (var i = 0; i < files.length; ++i) {
+        new DataRequest(files[i]['start'], files[i]['end'], files[i]['audio'] || 0).open('GET', files[i]['filename']);
+      }
+
+      function processPackageData(arrayBuffer) {
+        assert(arrayBuffer, 'Loading data file failed.');
+        assert(arrayBuffer instanceof ArrayBuffer, 'bad input to processPackageData');
+        var byteArray = new Uint8Array(arrayBuffer);
+        var curr;
+        // Reuse the bytearray from the XHR as the source for file reads.
+          DataRequest.prototype.byteArray = byteArray;
+          var files = metadata['files'];
+          for (var i = 0; i < files.length; ++i) {
+            DataRequest.prototype.requests[files[i].filename].onload();
+          }          Module['removeRunDependency']('datafile_embind11.data');
+
+      };
+      Module['addRunDependency']('datafile_embind11.data');
+
+      if (!Module.preloadResults) Module.preloadResults = {};
+
+      Module.preloadResults[PACKAGE_NAME] = {fromCache: false};
+      if (fetched) {
+        processPackageData(fetched);
+        fetched = null;
+      } else {
+        fetchedCallback = processPackageData;
+      }
+
     }
-    const printErr = (text) => {
-      logeditor.replaceRange("ERROR: "+text+"\n", CodeMirror.Pos(logeditor.lastLine()))
+    if (Module['calledRun']) {
+      runWithFS();
+    } else {
+      if (!Module['preRun']) Module['preRun'] = [];
+      Module["preRun"].push(runWithFS); // FS is not initialized yet, wait for it
     }
 
+    }
+    loadPackage({"files": [{"filename": "/home/runner/micromamba/envs/emforgetest/lib/python3.11/site-packages/embind11.so", "start": 0, "end": 943446}], "remote_package_size": 943446, "package_uuid": "412f0ab4-b990-4527-8e1d-e7f91a493ac0"});
 
-    window.onload = () => {
-        var savedText = localStorage.getItem("text") || "";
-        editor.getDoc().setValue(savedText);
-    };
-
-
-
-    var Module = {}
-    createModule({
-      print: print,
-      printErr:printErr,
-      // stderr: stderr,
-    })
-      .then((themodule) => {
-        Module = themodule
-
-
-        const delay = ms => new Promise(res => setTimeout(res, ms));
-
-
-        console.log("python_data NEW ..")
-        import('./python_data.js').then(datamodule => {
-            console.log("directories mounted")
-
-
-            console.log("delay")
-            delay(5000).then(()=>{
-              console.log("in delay then")
-
-              console.log("embind11 ..")
-              import('./embind11.js').then(datamodule => {
-                console.log("libs mounted")
-                console.log("Initializing interpreter")
-
-                themodule.initialize_interpreter()
-
-
-                let btn2 = document.createElement("button");
-                btn2.innerHTML = "run-try-catch";
-                btn2.onclick = function () {
-                  logeditor.getDoc().setValue("")
-                  var text = editor.getValue();
-                  localStorage.setItem("text", text)
-
-                  try{
-                    themodule.exec_code(text)
-                  }
-                  catch (e) {
-                    console.log("ERROR CATCHED",e)
-                    logeditor.replaceRange("JS-ERROR: "+String(e)+"\n", CodeMirror.Pos(logeditor.lastLine()))
-                  }
-                };
-                document.body.appendChild(btn2);
-
-              });
-            })
-        });
-
-
-
-        
-      });
-  </script>
-</body>
-</html>
+  })();
